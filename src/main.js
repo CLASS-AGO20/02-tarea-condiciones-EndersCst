9597732a673dc09d63987d860b3ecbd2a1e7c116
EndersCst <73366481+EndersCst@users.noreply.github.com>
2020-11-27 20:42:16 -0600

    implemented esPar

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 export default class App {
- 
+
+esPar (numero) {
+
+if(numero == 0) {
+return false;
+} else if (numero % 2 == 0) {
+return true;    
+} else {
+return false;
+} 
+}
 }
 
 let app = new App();
+
